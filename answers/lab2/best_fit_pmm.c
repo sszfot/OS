31dbcf82d31dac773b79b4f44dd5bee12ca0cacf
Foredawn <154438682+Cockcrow-s@users.noreply.github.com>
2024-10-20 10:06:51 +0800

    Update best_fit_pmm.c

--- answers/lab2/best_fit_pmm.c
+++ answers/lab2/best_fit_pmm.c
@@ -76,7 +76,7 @@ best_fit_init_memmap(struct Page* base, size_t n) {
         assert(PageReserved(p));
         p->flags = p->property = 0;  // 重置 flags 和 property
         set_page_ref(p, 0);  // 重置引用计数
-        /*LAB2 EXERCISE 2: YOUR CODE*/
+        /*LAB2 EXERCISE 2: 2210585  2210587*/
         // 清空当前页框的标志和属性信息，并将页框的引用计数设置为0
     }
     base->property = n;
@@ -97,7 +97,7 @@ best_fit_init_memmap(struct Page* base, size_t n) {
         if (le == &free_list) {  // 如果遍历结束还没有插入，则添加到链表尾部
             list_add(le, &(base->page_link));
         }
-        /*LAB2 EXERCISE 2: YOUR CODE*/
+        /*LAB2 EXERCISE 2: 2210585  2210587*/
        // 编写代码
        // 1、当base < page时，找到第一个大于base的页，将base插入到它前面，并退出循环
        // 2、当list_next(le) == &free_list时，若已经到达链表结尾，将base插入到链表尾部
@@ -115,7 +115,7 @@ best_fit_alloc_pages(size_t n) {
     list_entry_t* le = &free_list;
     size_t best_size = nr_free + 1;
 
-    /*LAB2 EXERCISE 2: YOUR CODE*/
+    /*LAB2 EXERCISE 2: 2210585 2210587*/
    // 下面的代码是first-fit的部分代码，请修改下面的代码改为best-fit
    // 遍历空闲链表，查找满足需求的空闲页框
    // 如果找到满足需求的页面，记录该页面以及当前找到的最小连续空闲页框数量
@@ -154,7 +154,7 @@ best_fit_free_pages(struct Page* base, size_t n) {
         p->flags = 0;
         set_page_ref(p, 0);
     }
-    /*LAB2 EXERCISE 2: YOUR CODE*/
+    /*LAB2 EXERCISE 2: 2210585  2210587*/
     // 编写代码
     // 具体来说就是设置当前页块的属性为释放的页块数、并将当前页块标记为已分配状态、最后增加nr_free的值
     base->property = n;  // 设置属性
@@ -187,7 +187,7 @@ best_fit_free_pages(struct Page* base, size_t n) {
             list_del(&(base->page_link));  // 从链表中删除
             base = p;  // 更新 base 为合并后的块
         }
-        /*LAB2 EXERCISE 2: YOUR CODE*/
+        /*LAB2 EXERCISE 2: 2210585  2210587*/
          // 编写代码
         // 1、判断前面的空闲页块是否与当前页块是连续的，如果是连续的，则将当前页块合并到前面的空闲页块中
         // 2、首先更新前一个空闲页块的大小，加上当前页块的大小
