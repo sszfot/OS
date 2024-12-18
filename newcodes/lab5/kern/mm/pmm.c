b0a54e768eccb0651171029af23138f41a5a900c
Foredawn <154438682+Cockcrow-s@users.noreply.github.com>
2024-12-18 23:54:49 +0800

    Update pmm.c

--- newcodes/lab5/kern/mm/pmm.c
+++ newcodes/lab5/kern/mm/pmm.c
@@ -370,7 +370,7 @@ int copy_range(pde_t *to, pde_t *from, uintptr_t start, uintptr_t end,
             assert(page != NULL);
             assert(npage != NULL);
             int ret = 0;
-            /* LAB5:EXERCISE2 YOUR CODE
+            /* LAB5:EXERCISE2 2210585  2210587
              * replicate content of page to npage, build the map of phy addr of
              * nage with the linear addr start
              *
@@ -388,7 +388,10 @@ int copy_range(pde_t *to, pde_t *from, uintptr_t start, uintptr_t end,
              * (3) memory copy from src_kvaddr to dst_kvaddr, size is PGSIZE
              * (4) build the map of phy addr of  nage with the linear addr start
              */
-
+            uintptr_t* src = page2kva(page);
+            uintptr_t* dst = page2kva(npage);
+            memcpy(dst, src, PGSIZE);
+            ret = page_insert(to, npage, start, perm);
 
             assert(ret == 0);
         }
@@ -625,4 +628,3 @@ static int get_pgtable_items(size_t left, size_t right, size_t start,
     }
     return 0;
 }
-
